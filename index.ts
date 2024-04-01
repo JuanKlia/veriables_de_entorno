ea88da0bace8e4bf1d78e166cdb526c213225ac2
unknown <juankliabaras@gmail.com>
2024-03-31 22:44:04 -0300

    Segundo commit

--- index.ts
+++ index.ts
@@ -2,6 +2,11 @@ import * as express from "express";
 
 const app = express();
 
+if (process.env.ENVIRONMENT == "dev") {
+  console.log("Entro dev");
+} else if (process.env.ENVIRONMENT == "prod") {
+  console.log("Entro prod");
+}
 app.get("/prueba", (req, res) => {
   res.json("Ok");
 });
